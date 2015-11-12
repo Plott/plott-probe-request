a7226b015832e186dc0c1caf57ca8f79e235835a
cwhite911 <smortopahri@gmail.com>
2015-11-11 22:58:29 -0500

    Change tcpdump cmd

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var spawn = require('child_process').spawn;
  */
 
 module.exports = function(callback) {
-  var tcpdump = spawn('tcpdump', ['sudo tcpdump -I -e -i wlan1 -s 256 type mgt subtype probe-req']);
+  var tcpdump = spawn('tcpdump', ['tcpdump -I -e -i wlan1 -s 256 type mgt subtype probe-req']);
 
   tcpdump.stdout.on('data', function (data) {
     console.log('tcpdump data: ' + data);
@@ -38,4 +38,4 @@ module.exports = function(callback) {
     console.log('Failed to start child process.');
     return callback(err, null);
   });
-}
+};
